16748ffb4a4743777d3f8f0941a57717d80d0fb7
Lulupointu <lucasdelsol@hotmail.fr>
2021-02-16 19:03:36 +0100

    Remove dart:io import

--- lib/src/move_to_background_fake.dart
+++ lib/src/move_to_background_fake.dart
@@ -1,7 +1,5 @@
-import 'dart:io' if (dart.library.io) 'dart:html';
-
 class MoveToBackground {
 
   /// This should not be called
-  static Future<void> moveTaskToBack() async => throw('This method should only be called on IOS or Android but current platform is ${Platform.operatingSystem} not IOS or Android.');
+  static Future<void> moveTaskToBack() async => throw('This method should only be called on IOS or Android.');
 }
