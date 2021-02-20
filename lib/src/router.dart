79e6434b273c1c189bb6225b1bc31bb642e97f3c
Lulupointu <lucasdelsol@hotmail.fr>
2021-02-20 15:07:46 +0100

    Taking path parameters into account when trying to find the right named route

--- lib/src/router.dart
+++ lib/src/router.dart
@@ -837,11 +837,27 @@ class VRouterState extends State<VRouter> {
     Map<String, String> newState = const {},
     bool isReplacement = false,
   }) {
-    // Find the path corresponding to the name
-    var newPath = pathToRoutes
-        .firstWhere((_VRoutePath vRoutePathRegexp) => (vRoutePathRegexp.name == name),
-            orElse: () => throw Exception('Could not find [VRouteElement] with name $name'))
-        .path;
+    // Find the VRoutePath corresponding to the name
+    // Since each alias represent a VRoutePath, there might be several element in the list
+    var potentialRoutes = pathToRoutes
+        .where((_VRoutePath vRoutePathRegexp) => (vRoutePathRegexp.name == name))
+        .toList();
+
+    if (potentialRoutes.isEmpty) {
+      throw Exception('Could not find VRouteElement with name $name');
+    }
+
+    // Get the path from the list of potentialRoute
+    // To discriminate we find the one which pathParameters match the given pathParameters
+    var newPath = potentialRoutes.firstWhere(
+      (_VRoutePath vRoutePathRegexp) => (vRoutePathRegexp.parameters == pathParameters.keys),
+      orElse: () => throw Exception(
+        'Could not find a path with the exact path parameters ${pathParameters.keys}.\n'
+        'To navigate to a route named "$name", you must give one of the following list of path parameters:\n${[
+          for (var potentialRoute in potentialRoutes) '    - ${potentialRoute.parameters}'
+        ].join("\n")} ',
+      ),
+    ).path;
 
     // Encode the path parameters
     final encodedPathParameters = pathParameters.map<String, String>(
