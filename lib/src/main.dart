8c5d86c5050a1ba129dd01219d6d436b3b1a53e1
Lulupointu <lucasdelsol@hotmail.fr>
2021-02-23 13:04:59 +0100

    Implement custom Platform to display web badge on pub .dev

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -10,16 +10,27 @@ import 'package:flutter/widgets.dart';
 import 'package:path_to_regexp/path_to_regexp.dart';
 import 'package:simple_url_handler/simple_url_handler.dart';
 import 'package:url_strategy/url_strategy.dart';
+
 // import 'move_to_background.dart';
 import 'fake_web_helpers.dart' if (dart.library.js) 'web_helpers.dart';
-import 'dart:io' show Platform;
+import 'platform/platform_none.dart'
+    if (dart.library.io) 'platform/platform_io.dart'
+    if (dart.library.js) 'platform/platform_web.dart';
 
 part 'exceptions.dart';
+
 part 'route_element_widget.dart';
+
 part 'navigation_2_wrappers.dart';
+
 part 'navigation_guard.dart';
+
 part 'page.dart';
+
 part 'route.dart';
+
 part 'route_element.dart';
+
 part 'router.dart';
+
 part 'redirector.dart';
---
+++ lib/src/platform/platfrom_web.dart
@@ -0,0 +1,34 @@
+class Platform {
+
+  /// Whether the operating system is a version of
+  /// [Linux](https://en.wikipedia.org/wiki/Linux).
+  ///
+  /// This value is `false` if the operating system is a specialized
+  /// version of Linux that identifies itself by a different name,
+  /// for example Android (see [isAndroid]).
+  static final bool isLinux = false;
+
+  /// Whether the operating system is a version of
+  /// [macOS](https://en.wikipedia.org/wiki/MacOS).
+  static final bool isMacOS = false;
+
+  /// Whether the operating system is a version of
+  /// [Microsoft Windows](https://en.wikipedia.org/wiki/Microsoft_Windows).
+  static final bool isWindows = false;
+
+  /// Whether the operating system is a version of
+  /// [Android](https://en.wikipedia.org/wiki/Android_%28operating_system%29).
+  static final bool isAndroid = false;
+
+  /// Whether the operating system is a version of
+  /// [iOS](https://en.wikipedia.org/wiki/IOS).
+  static final bool isIOS = false;
+
+  /// Whether the operating system is a version of
+  /// [Fuchsia](https://en.wikipedia.org/wiki/Google_Fuchsia).
+  static final bool isFuchsia = false;
+
+  /// Whether the operating system is a version of
+  /// [Web](https://en.wikipedia.org/wiki/Web).
+  static final bool isWeb = true;
+}
