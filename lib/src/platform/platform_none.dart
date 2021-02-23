d7421877803af203a623185c08b8489c41870ad8
Lulupointu <lucasdelsol@hotmail.fr>
2021-02-23 13:05:17 +0100

    dartftm formating

--- lib/src/platform/platform_none.dart
+++ lib/src/platform/platform_none.dart
@@ -1,35 +1,33 @@
-
 class Platform {
-
   /// Whether the operating system is a version of
   /// [Linux](https://en.wikipedia.org/wiki/Linux).
   ///
   /// This value is `false` if the operating system is a specialized
   /// version of Linux that identifies itself by a different name,
   /// for example Android (see [isAndroid]).
-  static bool get isLinux => throw('Not Implemented');
+  static bool get isLinux => throw ('Not Implemented');
 
   /// Whether the operating system is a version of
   /// [macOS](https://en.wikipedia.org/wiki/MacOS).
-  static bool get isMacOS => throw('Not Implemented');
+  static bool get isMacOS => throw ('Not Implemented');
 
   /// Whether the operating system is a version of
   /// [Microsoft Windows](https://en.wikipedia.org/wiki/Microsoft_Windows).
-  static bool get isWindows => throw('Not Implemented');
+  static bool get isWindows => throw ('Not Implemented');
 
   /// Whether the operating system is a version of
   /// [Android](https://en.wikipedia.org/wiki/Android_%28operating_system%29).
-  static bool get isAndroid => throw('Not Implemented');
+  static bool get isAndroid => throw ('Not Implemented');
 
   /// Whether the operating system is a version of
   /// [iOS](https://en.wikipedia.org/wiki/IOS).
-  static bool get isIOS => throw('Not Implemented');
+  static bool get isIOS => throw ('Not Implemented');
 
   /// Whether the operating system is a version of
   /// [Fuchsia](https://en.wikipedia.org/wiki/Google_Fuchsia).
-  static bool get isFuchsia => throw('Not Implemented');
+  static bool get isFuchsia => throw ('Not Implemented');
 
   /// Whether the operating system is a version of
   /// [Web](https://en.wikipedia.org/wiki/Web).
-  static bool get isWeb => throw('Not Implemented');
+  static bool get isWeb => throw ('Not Implemented');
 }
