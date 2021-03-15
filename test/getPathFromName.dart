b1390929815c5c19ec732162f5e2bbc360562bb3
Lulupointu <lucasdelsol@hotmail.fr>
2021-03-15 13:02:00 +0100

    Testing nested named

--- test/getPathFromName.dart
+++ test/getPathFromName.dart
@@ -5,73 +5,73 @@ import 'package:vrouter/vrouter.dart';
 void main() {
   group(
     'The following tests are used to test [VRouteElement.getPathFromName]',
-        () {
-          test('Basic named', () {
-
-            final vRouter = VRouter(
-              routes: [
-                VWidget(
-                  widget: Container(),
-                  path: '/home',
-                  name: 'home',
-                  subroutes: [
-                    VWidget(
-                      widget: Container(),
-                      path: '/other',
-                      name: 'other'
-                    ),
-                  ],
-                ),
-              ],
-            );
-
-            final newPath = vRouter
-                .getPathFromName(
-              elementToPop,
-              remainingPathParameters: {},
-              pathParameters: {},
-              parentPath: null,
-            )
-                ?.path;
-
-            expect(newPath, '/home');
-          });
-
-
-          test('Nested named', () {
-            final elementToPop = VWidget(
+    () {
+      test('Basic named', () {
+        final vRouter = VRouter(
+          routes: [
+            VWidget(
               widget: Container(),
-              path: '/settings',
-              subroutes: [],
-            );
-
-            final vRouter = VRouter(
-              routes: [
+              path: '/home',
+              name: 'home',
+              subroutes: [
                 VWidget(
                   widget: Container(),
-                  path: '/home',
+                  path: '/other',
+                  name: 'other',
+                ),
+              ],
+            ),
+          ],
+        );
+
+        final newPath = vRouter.getPathFromName(
+          'home',
+          remainingPathParameters: {},
+          pathParameters: {},
+          parentPath: null,
+        );
+
+        expect(newPath, '/home');
+      });
+
+      test('Nested named', () {
+        final vRouter = VRouter(
+          routes: [
+            VWidget(
+              widget: Container(),
+              path: '/home',
+              name: 'home',
+              subroutes: [
+                VWidget(
+                  widget: Container(),
+                  path: 'other',
+                  name: 'other',
                   subroutes: [
-                    elementToPop,
                     VWidget(
                       widget: Container(),
-                      path: '/other',
+                      path: 'settings',
+                      name: 'settings',
                     ),
                   ],
                 ),
+                VWidget(
+                  widget: Container(),
+                  path: 'aa',
+                ),
               ],
-            );
+            ),
+          ],
+        );
 
-            final newPath = vRouter
-                .getPathFromPop(
-              elementToPop,
-              pathParameters: {},
-              parentPath: null,
-            )
-                ?.path;
+        final newPath = vRouter.getPathFromName(
+          'other',
+          remainingPathParameters: {},
+          pathParameters: {},
+          parentPath: null,
+        );
 
-            expect(newPath, '/home');
-          });
-
+        expect(newPath, '/home/other');
+      });
     },
   );
 }
