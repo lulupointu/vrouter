8bf124228e546912eb167b9de82db16a7e931c38
Lulupointu <lucasdelsol@hotmail.fr>
2021-07-29 15:06:46 +0200

    Add test for VRedirector.toNamed with pathParameters

--- test/vredirector_test.dart
+++ test/vredirector_test.dart
@@ -320,5 +320,59 @@ main() {
       expect(vWidget2Finder, findsNothing);
       expect(vWidget3Finder, findsOneWidget);
     });
+
+    testWidgets('vRedirector.toNamed with path parameters', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        VRouter(
+          initialUrl: '/settings',
+          beforeLeave: (vRedirector, _) async => (vRedirector.newVRouterData?.names.contains('other') ?? false)
+              ? null
+              : vRedirector.toNamed('other', pathParameters: {'id': '2'}),
+          routes: [
+            VWidget(
+              path: '/',
+              widget: Text('VWidget1'),
+              stackedRoutes: [
+                VWidget(
+                  path: '/settings',
+                  widget: Builder(
+                    builder: (BuildContext context) => TextButton(
+                      onPressed: () => VRouter.of(context).to('/'),
+                      child: Text('VWidget2'),
+                    ),
+                  ),
+                ),
+                VWidget(
+                    path: '/other/:id', widget: Text('VWidget3'), name: 'other'),
+              ],
+            ),
+          ],
+        ),
+      );
+
+      await tester.pumpAndSettle();
+
+      // At first we are on "/settings" (because we set initialUrl)
+      // so only VWidget should be shown
+
+      final vWidget1Finder = find.text('VWidget1');
+      final vWidget2Finder = find.text('VWidget2');
+      final vWidget3Finder = find.text('VWidget3');
+
+      expect(vWidget1Finder, findsNothing);
+      expect(vWidget2Finder, findsOneWidget);
+      expect(vWidget3Finder, findsNothing);
+
+      // Try navigating to '/'
+      // Tap the add button.
+      await tester.tap(find.byType(TextButton));
+      await tester.pumpAndSettle();
+
+      // The navigation should have been redirected to '/other'
+      // So only VWidget should be visible
+      expect(vWidget1Finder, findsNothing);
+      expect(vWidget2Finder, findsNothing);
+      expect(vWidget3Finder, findsOneWidget);
+    });
   });
 }
